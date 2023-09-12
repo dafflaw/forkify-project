3d2d42d11c6d0afa05a550e02be58c90cdfac045
omad_d <odilshodvekov13@gmail.com>
2023-09-12 20:58:34 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '760c2a9f-d717-4e12-bc5e-2491668603a5';
+export const MODAL_CLOSE_SEC = 2.5;
